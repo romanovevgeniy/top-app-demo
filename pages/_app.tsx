b86cfec5be8db40160242947f201fbcc5e4b99b6
Evgeniy Romanov <romanov.evgeniy86@gmail.com>
2022-05-14 18:42:46 +0300

    adding fonts and colors to a project

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,19 @@
 import { AppProps } from 'next/app'
+import Head from 'next/head'
 import '../styles/globals.css'
+import React from 'react'
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
-    return <Component {...pageProps} />;
+    return <>
+        <Head>
+            <title>My Top - наш лучший топ</title>;
+            <link rel="icon" href="/favicon.ico" />
+            <link rel="preconnect" href="https://fonts.googleapis.com" />
+            <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+            <link href="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@300;400;500;700&display=swap" rel="stylesheet" />
+        </Head>
+        <Component {...pageProps} />
+    </>
 }
 
 export default MyApp;
