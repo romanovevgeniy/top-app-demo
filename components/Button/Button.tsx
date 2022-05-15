4d60aacb00bafbf37ecee5e64236b7646677cbfb
Evgeniy Romanov <romanov.evgeniy86@gmail.com>
2022-05-15 21:06:03 +0300

    button component configuration, add svg

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -1,15 +1,22 @@
 import styles from './Button.module.css';
 import { ButtonProps } from './Button.props';
+import ArrowIcon from './arrow.svg';
 import cn from 'classnames';
 
-export const Button = ({ appearance, children }: ButtonProps): JSX.Element => {
+export const Button = ({ appearance, arrow = 'none', children, className, ...props }: ButtonProps): JSX.Element => {
     return (
-        <button className={cn(styles.button, {
+        <button className={cn(styles.button, className, {
             [styles.primary]: appearance == 'primary',
-            [styles.ghost]: appearance == 'ghost'
+            [styles.ghost]: appearance == 'ghost',
         })}
+            {...props}
         >
             {children}
-        </button >
-    )
+            {arrow != 'none' && <span className={cn(styles.arrow, {
+                [styles.down]: arrow == 'down'
+            })}>
+                <ArrowIcon />
+            </span>}
+        </button>
+    );
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,8 +5,8 @@ import { Button, Htag } from '../components';
 export default function Home(): JSX.Element {
     return (
         <div>
-            <Htag tag='h1'>Текст</Htag>,
-            <Button appearance='primary'>Кнопка</Button>,
+            <Htag tag='h1'>Текст</Htag>
+            <Button appearance='primary'>Кнопка</Button>
             <Button appearance='ghost'>Кнопка</Button>
         </div>
     );
