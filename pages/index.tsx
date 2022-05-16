96e44bb87ad69dcc7754717c316c87432927514a
Evgeniy Romanov <romanov.evgeniy86@gmail.com>
2022-05-16 19:11:58 +0300

    add component p tag

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import { Button, Htag } from '../components';
+import { Button, Htag, P } from '../components';
 
 export default function Home(): JSX.Element {
     return (
@@ -8,6 +8,9 @@ export default function Home(): JSX.Element {
             <Htag tag='h1'>Текст</Htag>
             <Button appearance='primary' arrow='right'>Кнопка</Button>
             <Button appearance='ghost' arrow='right'>Кнопка</Button>
+            <P size='l'>Большой</P>
+            <P size='m'>Средний</P>
+            <P size='s'>Маленький</P>
         </div>
     );
-}
+};
