8db6c6823f99f3f420c72063441f905a590ef7b2
Evgeniy Romanov <romanov.evgeniy86@gmail.com>
2022-05-16 18:42:15 +0300

    adjusting button properties

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,8 +6,8 @@ export default function Home(): JSX.Element {
     return (
         <div>
             <Htag tag='h1'>Текст</Htag>
-            <Button appearance='primary'>Кнопка</Button>
-            <Button appearance='ghost'>Кнопка</Button>
+            <Button appearance='primary' arrow='right'>Кнопка</Button>
+            <Button appearance='ghost' arrow='right'>Кнопка</Button>
         </div>
     );
 }
