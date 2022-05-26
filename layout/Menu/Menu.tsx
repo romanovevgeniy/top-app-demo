5b80126f90a88e340f8f743a8bcdad48531bf7e0
Evgeniy Romanov <romanov.evgeniy86@gmail.com>
2022-05-26 12:47:07 +0300

    add menu interface and category menu

--- layout/Menu/Menu.tsx
+++ layout/Menu/Menu.tsx
@@ -3,6 +3,19 @@ import cn from 'classnames';
 import { format } from 'date-fns';
 import { useContext } from 'react';
 import { AppContext } from '../../context/app.context';
+import { FirstLevelMenuItem } from '../../interfaces/menu.interface';
+import CourcesIcon from './icons/cources.svg';
+import ServicesIcon from './icons/services.svg';
+import BooksIcon from './icons/books.svg';
+import ProductsIcon from './icons/products.svg';
+import { TopLevelCategory } from '../../interfaces/page.interface';
+
+const firstLevelMenuItem: FirstLevelMenuItem[] = [
+	{ route: 'cources', name: 'Курсы', icon: <CourcesIcon />, id: TopLevelCategory.Courses },
+	{ route: 'services', name: 'Сервисы', icon: <ServicesIcon />, id: TopLevelCategory.Services },
+	{ route: 'books', name: 'Книги', icon: <BooksIcon />, id: TopLevelCategory.Books },
+	{ route: 'products', name: 'Продукты', icon: <ProductsIcon />, id: TopLevelCategory.Products },
+];
 
 export const Menu = (): JSX.Element => {
 	const { menu, setMenu, firstCategory } = useContext(AppContext);
