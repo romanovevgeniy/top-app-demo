a83c87f9efd91a0e76994a41f4099ae52bc4995c
Evgeniy Romanov <romanov.evgeniy86@gmail.com>
2022-05-27 13:40:42 +0300

    routing structure

--- layout/Menu/Menu.tsx
+++ layout/Menu/Menu.tsx
@@ -1,24 +1,11 @@
 import styles from './Menu.module.css';
 import cn from 'classnames';
-import { format } from 'date-fns';
 import { useContext } from 'react';
 import { AppContext } from '../../context/app.context';
 import { FirstLevelMenuItem, PageItem } from '../../interfaces/menu.interface';
-import CourcesIcon from './icons/cources.svg';
-import ServicesIcon from './icons/services.svg';
-import BooksIcon from './icons/books.svg';
-import ProductsIcon from './icons/products.svg';
-import { TopLevelCategory } from '../../interfaces/page.interface';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
-
-
-const firstLevelMenuItem: FirstLevelMenuItem[] = [
-	{ route: 'cources', name: 'Курсы', icon: <CourcesIcon />, id: TopLevelCategory.Courses },
-	{ route: 'services', name: 'Сервисы', icon: <ServicesIcon />, id: TopLevelCategory.Services },
-	{ route: 'books', name: 'Книги', icon: <BooksIcon />, id: TopLevelCategory.Books },
-	{ route: 'products', name: 'Продукты', icon: <ProductsIcon />, id: TopLevelCategory.Products },
-];
+import { firstLevelMenu } from '../../helpers/helpers';
 
 export const Menu = (): JSX.Element => {
 	const { menu, setMenu, firstCategory } = useContext(AppContext);
@@ -36,7 +23,7 @@ export const Menu = (): JSX.Element => {
 	const buildFirstLevel = () => {
 		return (
 			<>
-				{firstLevelMenuItem.map(m => (
+				{firstLevelMenu.map(m => (
 					<div key={m.route}>
 						<Link href={`/${m.route}`}>
 							<a>
