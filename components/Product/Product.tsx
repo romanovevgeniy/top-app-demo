66332a7849871724c791c996f2bcd3dcea8e4af4
Evgeniy Romanov <romanov.evgeniy86@gmail.com>
2022-05-29 19:32:48 +0300

    add component divider

--- components/Product/Product.tsx
+++ components/Product/Product.tsx
@@ -5,20 +5,27 @@ import { Card } from '../Card/Card';
 import { Rating } from '../Rating/Rating';
 import { Tag } from '../Tag/Tag';
 import { Button } from '../Button/Button';
+import { priceRu } from '../../helpers/helpers';
+import { Divider } from '../Divider/Divider';
 
 export const Product = ({ product, className, ...props }: ProductProps): JSX.Element => {
 	return (
 		<Card className={styles.product}>
 			<div className={styles.logo}><img src={process.env.NEXT_PUBLIC_DOMAIN + product.image} alt={product.title} /></div>
 			<div className={styles.title}>{product.title}</div>
-			<div className={styles.price}>{product.price}</div>
-			<div className={styles.credit}>{product.credit}</div>
+			<div className={styles.price}>
+				{priceRu(product.price)}
+				{product.oldPrice && <Tag color='green' className={styles.oldPrice}>{priceRu(product.price - product.oldPrice)}</Tag>}
+			</div>
+			<div className={styles.credit}>
+				{priceRu(product.credit)}/<span className={styles.month}>мес</span>
+			</div>
 			<div className={styles.rating}><Rating rating={product.reviewAvg ?? product.initialRating} /></div>
 			<div className={styles.tags}>{product.categories.map(c => <Tag key={c} color='ghost'>{c}</Tag>)}</div>
 			<div className={styles.priceTitle}>цена</div>
 			<div className={styles.creditTitle}>кредит</div>
 			<div className={styles.rateTitle}>{product.reviewCount} отзывов</div>
-			<div className={styles.hr}><hr /></div>
+			<Divider className={styles.hr} />
 			<div className={styles.description}>{product.description}</div>
 			<div className={styles.feature}>Feature</div>
 			<div className={styles.advBlock}>
@@ -31,7 +38,7 @@ export const Product = ({ product, className, ...props }: ProductProps): JSX.Ele
 					<div>{product.disadvantages}</div>
 				</div>
 			</div>
-			<div className={styles.hr}><hr /></div>
+			<Divider className={styles.hr} />
 			<div className={styles.actions}>
 				<Button appearance='primary'>Узнать подробнее</Button>
 				<Button appearance='ghost' arrow='right'>Читать отзывы</Button>
