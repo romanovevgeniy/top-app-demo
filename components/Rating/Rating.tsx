469211c49d32e0fbc2a1b1ef716e96cb5e6d7de8
Evgeniy Romanov <romanov.evgeniy86@gmail.com>
2022-06-01 11:17:47 +0300

    add error handling on rating component

--- components/Rating/Rating.tsx
+++ components/Rating/Rating.tsx
@@ -3,8 +3,9 @@ import styles from './Rating.module.css';
 import cn from 'classnames';
 import { ForwardedRef, forwardRef, KeyboardEvent, useEffect, useState } from 'react';
 import StarIcon from './star.svg';
+import { error } from 'console';
 
-export const Rating = forwardRef(({ isEditable = false, rating, setRating, ...props }: RatingProps, ref: ForwardedRef<HTMLDivElement>): JSX.Element => {
+export const Rating = forwardRef(({ isEditable = false, rating, error, setRating, ...props }: RatingProps, ref: ForwardedRef<HTMLDivElement>): JSX.Element => {
 
 	const [ratingArray, setRatingArray] = useState<JSX.Element[]>(new Array(5).fill(<></>));
 
@@ -57,8 +58,11 @@ export const Rating = forwardRef(({ isEditable = false, rating, setRating, ...pr
 	}
 
 	return (
-		<div {...props} ref={ref}>
+		<div {...props} ref={ref} className={cn(styles.ratingWrapper, {
+			[styles.error]: error
+		})}>
 			{ratingArray.map((r, i) => (<span key={i}>{r}</span>))}
+			{error && <span className={styles.errorMessage}>{error.message}</span>}
 		</div>
 	)
 }); 
