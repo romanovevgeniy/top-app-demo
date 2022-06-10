de3485cfe79609d0a186e7f8041625d7fc4a6c03
Evgeniy Romanov <romanov.evgeniy86@gmail.com>
2022-06-10 16:14:46 +0300

    add dynamic icon

--- components/Up/Up.tsx
+++ components/Up/Up.tsx
@@ -4,6 +4,7 @@ import UpIcon from './up.svg';
 import { useScrollY } from '../../hooks/useScrollY';
 import { motion, useAnimation } from 'framer-motion';
 import { useEffect } from 'react';
+import { ButtonIcon } from '../ButtonIcon/ButtonIcon';
 
 export const Up = (): JSX.Element => {
 	const controls = useAnimation();
@@ -21,13 +22,12 @@ export const Up = (): JSX.Element => {
 	};
 
 	return (
-		<motion.button
+		<motion.div
 			className={styles.up}
-			onClick={scrollToTop}
 			animate={controls}
 			initial={{ opacity: 0 }}
 		>
-			<UpIcon />
-		</motion.button>
+			<ButtonIcon appearance='primary' icon='up' onClick={scrollToTop} />
+		</motion.div>
 	)
 };
