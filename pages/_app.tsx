fc6ba131db5f492771a1c0aad34003d6423f5325
Evgeniy Romanov <romanov.evgeniy86@gmail.com>
2022-06-11 22:56:15 +0300

    fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,8 +8,8 @@ function MyApp({ Component, pageProps }: AppProps): JSX.Element {
 		<Head>
 			<title>My Top - наш лучший топ</title>;
 			<link rel="icon" href="/favicon.ico" />
-/* подключаем шрифты с Google Fonts */
-
+			<link rel="preconnect" href="https://fonts.gstatic.com" />
+			<link href="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@300;400;500;700&display=swap" rel="stylesheet" />
 		</Head>
 		<Component {...pageProps} />
 	</>;
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,9 +10,6 @@ class MyDocument extends Document {
 		return (
 			<Html lang='ru'>
 				<Head />
-				<link rel="preconnect" href="https://fonts.googleapis.com" />
-				<link rel="preconnect" href="https://fonts.gstatic.com" />
-				<link href="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@300;400;500;700&display=swap" rel="stylesheet" />
 				<body>
 					<Main />
 					<NextScript />
